0b1d69534c21a3fef5883a7dd680231e85ffa5bc
Drysandwich34 <rodriguezspencer11@gmail.com>
2025-10-28 16:03:47 -0500

    Fix wallpaper file extension in gnome-settings.sh

--- gnome/gnome-settings.sh
+++ gnome/gnome-settings.sh
@@ -2,6 +2,6 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
-echo "setting wallpaper to ${SCRIPT_DIR}/wallpaper.jpeg"
+echo "setting wallpaper to ${SCRIPT_DIR}/wallpaper.jpg"
 gsettings set org.gnome.desktop.background picture-uri-dark "file://${SCRIPT_DIR}/wallpaper.jpg"
 gsettings set org.gnome.desktop.background picture-options 'zoom'
